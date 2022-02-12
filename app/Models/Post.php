875d99d61db0f6a195f1d0dfd8ab23bd411bdc2a
Mohammadreza Rahimi <m.rahimi@azmagroup.ir>
2022-02-12 10:59:03 +0330

    Add post migration and model:
    Add posts migration.
    Add Post model which has one to many relation with user model.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,56 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use AdminUtil;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, AdminUtil;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'body',
+        'images',
+        'view',
+        'approved',
+    ];
+
+    /**
+     * One to many with post (inverse)
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Return current post slug
+     *
+     * @return string
+     */
+    public function path(): string
+    {
+        return "/posts/{$this->slug}";
+    }
+
+    /**
+     * Check post status (approved by admin)
+     *
+     * @return boolean
+     */
+    public function isApproved(): bool
+    {
+        return (bool) $this->approved;
+    }
 }
