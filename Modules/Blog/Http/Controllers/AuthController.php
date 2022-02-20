43053d444d6a059e3950a443a8d1dddc74d39d43
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2022-02-21 01:29:55 +0330

    Add Client Authentication:
    Add Passport Package.
    Add AuthController in Blog module.
    Add auth routes in Blog module.
    Add Passport routes in AuthServiceProvider.
    Add Passport apit guard in auth config.
    Add Passport trait with alias name to User model.

---
+++ Modules/Blog/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Blog\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Support\Renderable;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        User::create([
+           'name' => $request->get('name'),
+           'family' => $request->get('family'),
+           'mobile' => $request->get('mobile'),
+           'password' => Hash::make($request->get('password')),
+        ]);
+
+        return response()->json([
+            'message' => 'ثبت نام با موفقیت انجام شد'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('mobile', $request->get('mobile'))->first();
+
+        if (! $user || ! Hash::check($request->get('password'), $user->password)) {
+            return response()->json([
+                'errors' => [
+                    'mobile' => 'اطلاعات ورود اشتباه است',
+                ]
+            ], 403);
+        }
+
+        $token = $user->createToken('Client Access Token')->accessToken;
+
+        return response()->json([
+            'token'  => $token,
+            'name'   => $user->name,
+            'family' => $user->family,
+            'mobile' => $user->mobile,
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Passport\HasApiTokens as ClientApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
+    use ClientApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
