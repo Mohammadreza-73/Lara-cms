2ad47be829ec4667986aa8f49281f71e4e7ebc6c
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2022-02-26 01:10:05 +0330

    Modify layouts:
    Modify app layout
    Add favicon
    Add web.config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'CMS') }}</title>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
@@ -27,15 +27,6 @@
             @livewire('navigation-menu')
 
             @include('sections.header')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
             <!-- Page Content -->
             <main id="content-wrap h-100">
